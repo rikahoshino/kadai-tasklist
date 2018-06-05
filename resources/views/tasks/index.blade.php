184b686d06509394095076be18baf731457ca1b5
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-06-05 06:57:14 +0000

    bootstrap design

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,16 +2,29 @@
 
 @section('content')
 
-<h1>Task一覧</h1>
+    <h1>タスク一覧</h1>
 
-@if (count($tasks)>0)
-<ul>
-    @foreach($tasks as $task)
-    <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}  : {{ $task->content }}:<tasks class="show"></tasks> {{ $task->status }}</li>
-    @endforeach
-</ul>
-@endif
-
-{!! link_to_route('tasks.create', '新規タスクの作成') !!}
+@if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>ステータス</th>
+                    <th>タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+     {!! link_to_route('tasks.create', '新規タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
