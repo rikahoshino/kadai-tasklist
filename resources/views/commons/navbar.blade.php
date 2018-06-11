8331aee71a128a4d67c9ee8d5b42d474700a140e
rika.hoshino <ec2-user@ip-172-31-37-36.us-east-2.compute.internal>
2018-06-11 05:50:18 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,13 +8,13 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">Tasklists</a>
+                <a class="navbar-brand" href="/">Tasklist</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
                     
-                        <li>{!! link_to_route('users.index', 'Users') !!}</li>
+                       
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                             <ul class="dropdown-menu">
@@ -25,7 +25,7 @@
                         </li>
                     @else
                         <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
-                       
+                        <li>{!! link_to_route('login', 'Login') !!}</li>
                     @endif
                 </ul>
             </div>
